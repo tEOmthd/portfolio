c8c7ed3c38b802b5e755a1f3800b8cffa431cd4d
teo mathiaud <teo.mathiaud@etu.univ-amu.fr>
2025-02-24 15:45:15 +0100

    Edit du menu principal

--- my-app/src/components/Hero.jsx
+++ my-app/src/components/Hero.jsx
@@ -4,9 +4,9 @@ function Hero() {
   return (
     <section id="hero" className="hero">
       <div className="hero-container">
-        <h1>Bonjour, je suis <span className="highlight">Votre Nom</span></h1>
-        <h2>Développeur Web Frontend</h2>
-        <p>Je crée des expériences web interactives et responsives</p>
+        <h1>Bonjour, je suis <span className="highlight">Mathiaud Téo </span></h1>
+        <h2>Etudiant Informatique</h2>
+        <p>Compétent et rigoureux, l'étudiant dont vous avez besoin</p>
         <div className="hero-btns">
           <a href="#projects" className="btn btn-primary">Voir mes projets</a>
           <a href="#contact" className="btn btn-secondary">Me contacter</a>
