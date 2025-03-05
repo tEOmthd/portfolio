bc9262c4bef507610a9e23b9ca47f03c8c4459a2
teo mathiaud <teo.mathiaud@etu.univ-amu.fr>
2025-03-05 08:23:46 +0100

    Réarangement du code css

--- my-app/src/components/Contact.jsx
+++ my-app/src/components/Contact.jsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import "@fortawesome/fontawesome-free/css/all.min.css";
-
+import "../style/Contact.css"
 function Contact() {
   return (
     <section id="contact" className="contact">
